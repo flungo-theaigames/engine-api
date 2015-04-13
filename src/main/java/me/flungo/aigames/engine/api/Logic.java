35b7cd04937fd3666520de30fec69e67dfc28c34
Fabrizio <fab@lungo.co.uk>
2015-04-13 08:04:54 +0100

    Updated license headers.

--- src/main/java/me/flungo/aigames/engine/api/Logic.java
+++ src/main/java/me/flungo/aigames/engine/api/Logic.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2015 theaigames.com <developers@theaigames.com>.
+ * Copyright 2015 theaigames.com <developers@theaigames.com>,
+ * Copyright 2015 Fabrizio Lungo <fab@lungo.co.uk>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
